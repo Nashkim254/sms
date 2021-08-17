7f7f5c2db0705581f6f53ff93976c9fa087520dd
unknown <kimutainashon@gmail.com>
2021-08-17 22:12:41 +0300

    changes

---
+++ Medical.java
@@ -0,0 +1,17 @@
+
+package medical;
+
+import javax.swing.JFrame;
+
+
+public class Medical {
+
+   
+    public static void main(String[] args) {
+     
+        
+          RegistrationForm mf=new RegistrationForm();
+  
+    }
+    
+}
